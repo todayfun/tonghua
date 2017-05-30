793e804f91fb7514866656e1f320f7d6bd7834fb
boyi <229753540@qq.com>
2017-05-30 09:15:14 +0800

    simplify list page

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -2,7 +2,7 @@ class StocksController < ApplicationController
   # GET /stocks
   # GET /stocks.json
   def index
-    @stocks = Stock.where("weekrise=1 and monthrise=1")
+    @stocks = Stock.where(weekrise:1)#.where(monthrise:1)
 
     respond_to do |format|
       format.html # index.html.erb
