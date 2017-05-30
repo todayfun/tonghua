52fecd6d659d4aaccbb067975faff2c2189ad6dc
boyi <229753540@qq.com>
2017-05-30 19:23:36 +0800

    add rise type

--- app/helpers/stocks_helper.rb
+++ app/helpers/stocks_helper.rb
@@ -6,4 +6,17 @@ module StocksHelper
       "http://gu.qq.com/#{code}/gp"
     end
   end
+
+  def riselabel(weekrise)
+    case weekrise
+      when 1
+        "(连续上涨)"
+      when 2
+        "(超跌反弹)"
+      when 3
+        "(反弹上涨)"
+      else
+        ""
+    end
+  end
 end
