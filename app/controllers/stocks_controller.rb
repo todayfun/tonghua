2c0d6b4dc5ee71b4cee9f792e60974c6c25c89f6
boyi <229753540@qq.com>
2017-06-17 11:56:08 +0800

    add quarter chart

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -17,7 +17,7 @@ class StocksController < ApplicationController
   def show
     @stock = Stock.find(params[:id])
 
-    @fin_reports = FinReport.where(fd_code:@stock.code).order("fd_repdate desc")
+    @fin_reports = FinReport.where(fd_code:@stock.code).order("fd_repdate desc").all
 
     fy_matrix = {fd_year:[],fd_profit_base_share:[],fd_cash_base_share:[],fd_debt_rate:[]}
 
@@ -30,6 +30,18 @@ class StocksController < ApplicationController
       fy_matrix[:fd_debt_rate] << stkholder_rights_of_debt(r.fd_non_liquid_debts,r.fd_stkholder_rights)
     end
 
+    q_matrix = {fd_repdate:[],fd_profit_base_share:[],fd_cash_base_share:[],fd_debt_rate:[]}
+    cnt = 0
+    @fin_reports.each do |r|
+      q_matrix[:fd_repdate] << "#{r.fd_repdate.to_date}-#{fin_report_label r.fd_type}"
+      q_matrix[:fd_profit_base_share] << r.fd_profit_base_share
+      q_matrix[:fd_cash_base_share] << cash_base_share(@stock.stamp,@stock.gb,r.fd_cash_and_deposit)
+      q_matrix[:fd_debt_rate] << stkholder_rights_of_debt(r.fd_non_liquid_debts,r.fd_stkholder_rights)
+
+      cnt += 1
+      break if cnt > 4
+    end
+
     fd_years = fy_matrix[:fd_year].reverse
     if fd_years.blank?
       @fy_chart = nil
@@ -37,12 +49,26 @@ class StocksController < ApplicationController
       @fy_chart = LazyHighCharts::HighChart.new('graph') do |f|
         f.title(text: "年报-关键指标")
         f.xAxis(categories: fd_years)
-        f.yAxis(title:{text:"每股收益(元)"})
         f.legend(layout:"vertical",align:"right",verticalAlign:"middle")
 
         f.series(name:"每股收益(元)",data:fy_matrix[:fd_profit_base_share].reverse)
         f.series(name:"每股现金(元)",data:fy_matrix[:fd_cash_base_share].reverse)
         f.series(name:"股东权益占比",data:fy_matrix[:fd_debt_rate].reverse)
+      end
+    end
+
+    q_arr = q_matrix[:fd_repdate].reverse
+    if q_arr.blank?
+      @q_chart = nil
+    else
+      @q_chart = LazyHighCharts::HighChart.new('graph') do |f|
+        f.title(text: "季报-关键指标")
+        f.xAxis(categories: q_arr)
+        f.legend(layout:"vertical",align:"right",verticalAlign:"middle")
+
+        f.series(name:"每股收益(元)",data:q_matrix[:fd_profit_base_share].reverse)
+        f.series(name:"每股现金(元)",data:q_matrix[:fd_cash_base_share].reverse)
+        f.series(name:"股东权益占比",data:q_matrix[:fd_debt_rate].reverse)
       end
     end
 
