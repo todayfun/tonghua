9cd76201d92e4a2ad133b7840d58e218a7bd0240
boyi <229753540@qq.com>
2017-12-10 14:00:41 +0800

    refactor

--- app/helpers/stocks_helper.rb
+++ app/helpers/stocks_helper.rb
@@ -10,13 +10,13 @@ module StocksHelper
   def riselabel(stock)
     case stock.weekrise
       when 1
-        "(连续上涨)#{stock.weekrise}"
+        "连续上涨#{stock.weekrise}周"
       when 2
-        "(跌后反弹)#{stock.weekrise}"
+        "跌后反弹#{stock.weekrise}周"
       when 3
-        "(超跌反弹)#{stock.weekrise}"
+        "超跌反弹#{stock.weekrise}周"
       when 4
-        "(反弹上涨)#{stock.weekrise}"
+        "反弹上涨#{stock.weekrise}周"
       else
         ""
     end
@@ -25,6 +25,6 @@ module StocksHelper
   def rise_tags(stock)
     tags = (stock.good).map{|k,v| "#{k}:#{v}"};
     tags << riselabel(stock);
-    tags << stock.rate_of_profit
+    tags << "净利润#{stock.rate_of_profit}%"
   end
 end
